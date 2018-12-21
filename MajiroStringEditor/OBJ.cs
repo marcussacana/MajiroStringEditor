3f75f33e17560ad2ae7294e8ad927ec2ff11ee23
= <ddtank.marcus@gmail.com>
2018-12-21 14:51:22 -0200

    Fix Possible Bug

--- MajiroStringEditor/OBJ.cs
+++ MajiroStringEditor/OBJ.cs
@@ -51,7 +51,7 @@ namespace MajiroStringEditor
                     default:
                         if (!DialFinished) {
                             DialFinished = true;
-                            EndMap[Ind] = i - 2;
+                            EndMap[Ind] = i;
                         }
 
                         i++;
